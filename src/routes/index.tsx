221b93f2751bd9af82869f2ffa442ec1b3901674
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-09-05 15:34:36 +0100

    Add server function to index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
 
 export const Route = createFileRoute('/')({
+  loader: () => getMessage(),
   component: Home,
 })
 
+const getMessage = createServerFn().handler(() => {
+  return `Running in ${navigator.userAgent}`
+})
+
 function Home() {
+  const message = Route.useLoaderData()
+
   return (
     <div className="p-2">
-      <h3>Welcome Home!!!</h3>
+      <h3>{message}</h3>
     </div>
   )
 }
