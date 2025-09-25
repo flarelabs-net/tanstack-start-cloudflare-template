2406962a5df4763cf9d49542a54677791d62e9e5
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-09-25 18:29:48 +0100

    Use importable env

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
+import { env } from 'cloudflare:workers'
 
 export const Route = createFileRoute('/')({
-  loader: () => getMessage(),
+  loader: () => getData(),
   component: Home,
 })
 
-const getMessage = createServerFn().handler(() => {
-  return `Running in ${navigator.userAgent}`
+const getData = createServerFn().handler(() => {
+  return {
+    message: `Running in ${navigator.userAgent}`,
+    myVar: env.MY_VAR,
+  }
 })
 
 function Home() {
-  const message = Route.useLoaderData()
+  const data = Route.useLoaderData()
 
   return (
     <div className="p-2">
-      <h3>{message}</h3>
+      <p>{data.message}</p>
+      <p>{data.myVar}</p>
     </div>
   )
 }
